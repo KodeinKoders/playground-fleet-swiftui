a61979e38312d3d8c8966a0079335ed1363727ea
Romain Boisselle <romain.boisselle@proton.me>
2023-11-24 10:13:53 +0100

    Initial Commit

---
+++ Fleet SwiftUI Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fleet SwiftUI Tutorial
+//
+//  Created by Romain Boisselle on 24/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fleet SwiftUI Tutorial/Fleet_SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_SwiftUI_TutorialApp.swift
+//  Fleet SwiftUI Tutorial
+//
+//  Created by Romain Boisselle on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fleet_SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
